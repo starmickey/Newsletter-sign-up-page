fa7302fdf540b388175ec06db9a77baf4adbdbbe
starmickey <micaelaestrellamorales@gmail.com>
2023-01-21 21:25:04 -0300

    port that's listened by the app was changed

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ app.post("/failure", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen(3000, function () {
-    console.log("Server running in port 3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server running");
 });
